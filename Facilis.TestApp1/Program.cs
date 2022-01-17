8baaef1873248b251eb1c212c0ebbdf1db128c5c
Jake Young <JakeAYoung95@gmail.com>
2022-01-16 21:13:17 -0600

    Cleanup

--- Facilis.TestApp1/Program.cs
+++ Facilis.TestApp1/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using Facilis.LineItems.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using Facilis.LineItems.Models;
 
 namespace Facilis.LineItems
 {
